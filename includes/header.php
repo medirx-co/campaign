09e06ccfc6e6e3a5aa3384a697848f7a00e1052a
Veena Rao <veenasrao5@gmail.com>
2022-08-18 13:01:41 +0530

    new_changes

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="" />
+		<meta name="author" content="" />
+		<meta name="robots" content="" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="MotaAdmin - Bootstrap Admin Dashboard" />
+		<meta property="og:title" content="MotaAdmin - Bootstrap Admin Dashboard" />
+		<meta property="og:description" content="MotaAdmin - Bootstrap Admin Dashboard" />
+		<meta property="og:image" content="social-image.png" />
+		<meta name="format-detection" content="telephone=no">
+
+		<title> Admin Dashboard</title>
+
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+		<link href="vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="vendor/chartist/css/chartist.min.css">
+		<link href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="../../cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
+		<link href="vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
+
+
+       
+	</head>
+	<body>
